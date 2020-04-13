5da9f6e39f6f64cbe3a0842fc5d8e5c0ab65ef15
Tyler Balson <tbalson@indiana.edu>
2020-04-13 14:26:41 -0400

    new server and dew dockerfile

--- server.py
+++ server.py
@@ -1,7 +1,4 @@
-"""
-Main module of the server file
-"""
-
+from flask import Flask
 from flask import jsonify
 import connexion
 
@@ -15,9 +12,9 @@ app.add_api("master.yaml")
 # create a URL route in our application for "/"
 @app.route("/")
 def home():
-    msg = {"msg": "It's working!"}
+    msg = {"server-msg": "It's working!"}
     return jsonify(msg)
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
